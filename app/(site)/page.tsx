ef4ff5485ce2ac748f018ad2e1cba7d2391a3382
CARTOON <cartoon@CARTOON.CARTOON>
2023-07-11 01:02:56 +0300

    globals

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -18,8 +18,19 @@ export default function Home() {
         height="48"
         width="48"
         className="mx-auto w-auto"
-        src="/Images/logo.png"
+        src="/images/logo.png"
         />
+        <h2
+        className="
+        mt-6
+        text-center
+        text-3xl
+        font-bold
+        tracking-tight
+        text-gray-900
+        ">
+          Sign in to your account
+        </h2>
       </div>
 
     </div>
